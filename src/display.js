a08116e01b0e81fa2e7325e5d7e92947794b0ca5
Michael <mikemcd.bbs@gmail.com>
2023-10-24 11:34:26 -0400

    Populate display controller inputs

--- src/display.js
+++ src/display.js
@@ -2,16 +2,79 @@ import "./style.css";
 
 const displayController = () => {
 
-    const createMain = () => {
-        
+    // Initialize page elements
+    const _main = (() => {
         const main = document.createElement("main");
-
-        const container = document.createElement("div");
-        container.id = "main-content";
-
-        main.append(container);
-        
         return main;
+    })();
+
+    const _mainContent = (() => {
+        const mainContent = document.createElement("form");
+        mainContent.id = "main-content";
+        return mainContent;
+    })();
+
+    const _paramContainer = (() => {
+        const container = document.createElement("div");
+        return container;
+    })();
+
+    const _param1 = (() => {
+        const label = document.createElement("label");
+        const param = document.createElement("input");
+        const container = document.createElement("div");
+
+        container.append(label, param);
+        return container;
+    })();
+
+    const _param2 = (() => {
+        const label = document.createElement("label");
+        const param = document.createElement("input");
+        const container = document.createElement("div");
+
+        container.append(label, param);
+        return container;
+    })();
+
+    const _param3 = (() => {
+        const label = document.createElement("label");
+        const param = document.createElement("input");
+        const container = document.createElement("div");
+
+        container.append(label, param);
+        return container;
+    })();
+
+    const _buttonContainer = (() => {
+        const container = document.createElement("div");
+        return container;
+    })();
+
+    const _button1 = (() => {
+        const button = document.createElement("button");
+        return button;
+    })();
+
+    const _button2 = (() => {
+        const button = document.createElement("button");
+        return button;
+    })();
+
+    const _button3 = (() => {
+        const button = document.createElement("button");
+        return button;
+    })();
+
+    // Order page elements
+    const createMain = () => {
+
+        _paramContainer.append(_param1, _param2, _param3);
+        _buttonContainer.append(_button1, _button2, _button3);
+        _mainContent.append(_paramContainer, _buttonContainer);
+        _main.append(_mainContent);
+        
+        return _main;
     }
     
     return {
